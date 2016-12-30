21f07de257722dbd99ecbff71014c21ac336f56e
tallguy9271 <drewoken@comcast.net>
2016-12-30 02:52:32 -0500

    - initial work sesh has working flipping of burgers

---
+++ Assets/NVR_Spawner_Button.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using NewtonVR;
+
+namespace NewtonVR.Example
+{
+    public class NVR_Spawner_Button : MonoBehaviour
+    {
+        public NVRButton Button;
+
+        public GameObject ToCopy;
+
+        public Transform SpawnLocation;
+
+        private void Update()
+        {
+            if (Button.ButtonDown)
+            {
+                GameObject newGo = GameObject.Instantiate(ToCopy);
+                newGo.transform.position = SpawnLocation.position;
+                newGo.transform.rotation = SpawnLocation.rotation;
+                newGo.transform.localScale = ToCopy.transform.lossyScale;
+            }
+        }
+    }
+}
